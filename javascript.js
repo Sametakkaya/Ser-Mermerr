239762a506d2c3c35fe803cbe89cecee3e523f87
Samet <102583793+Sametakkaya@users.noreply.github.com>
2022-04-04 11:30:33 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+$('.proWrap').on({
+    mouseenter: function() {
+      $(this).children(".proDetail").stop().show(300);
+      $(this).addClass("classShadow");
+    },
+    mouseleave: function() {
+      $(this).children(".proDetail").stop().hide(200);
+      $(this).removeClass("classShadow");
+    }
+  });
